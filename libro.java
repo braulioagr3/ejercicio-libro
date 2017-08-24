a4b01031e5e2f6ee158fb75488dd3d814be03d45
Braulio Alejandro García Rivera <braulioagr3@gmail.com>
2017-08-24 10:34:13 -0500

    Se añadieron los parametros de: Autor Libro Se modifico el constructor para recibir y asignar los parametros al crear el objeto
    Nota:
    Para que lea una cadena esta debe ser ingreseada entre comillas ""

---
+++ libro.java
@@ -0,0 +1,10 @@
+public class libro
+{
+    private String Titulo = "";
+    private String Autor = "";
+    public libro(String titulo, String autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+    }
+}
