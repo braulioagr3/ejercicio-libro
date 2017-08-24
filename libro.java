6421300b9d609665dc9e738988bfe7b6a7e785c9
braulioagr3 <31120570+braulioagr3@users.noreply.github.com>
2017-08-24 11:03:20 -0500

    Se añadieron el metodos de: dimeAutor dimeTitulo dimeDetalles

--- libro.java
+++ libro.java
@@ -2,17 +2,39 @@ public class libro
 {
     private String Titulo = "";
     private String Autor = "";
-    public libro(String titulo, String autor)
+    private int Paginas;
+    public libro(String titulo, String autor, int paginas)
     {
         Titulo = titulo;
         Autor = autor;
+        Paginas = paginas;
     }
+    
     public String dimeAutor()
     {
         return Autor;
     }
+    
     public String dimeTitulo()
     {
         return Titulo;
+    }
+    
+    public int dimePaginas()
+    {
+        return Paginas;
+    }
+    public String dimeDetalles()
+    {
+        String Detalles = "";
+        Detalles += "Titulo: ";
+        Detalles += Titulo;
+        Detalles += " ";
+        Detalles += "Autor: ";
+        Detalles += Autor;
+        Detalles += " ";
+        Detalles += "Paginas: ";
+        Detalles += Paginas;
+        return Detalles;
     }
 }
