9e32c7ab76bb90b4b783969851067362a8c47b2d
Braulio García <braulioagr3@gmail.com>
2017-08-31 22:51:18 -0500

    Se añadio la documentación con JavaDoc

--- libro.java
+++ libro.java
@@ -12,21 +12,34 @@ public class libro
         Paginas = paginas;
         Prestamos = 0;
     }
-    
+    /**
+     * Permite registrar el nombre del Autor
+     * @return Regresa el nomrbe del autor
+     */
     public String dimeAutor()
     {
         return Autor;
     }
-    
+    /**
+     * Permite registrar el nombre del Libro
+     * @return Regresa el nomrbe del libro
+     */
     public String dimeTitulo()
     {
         return Titulo;
     }
-    
+    /**
+     * Permite registrar el numero de Paginas
+     * @return Regresa el numero de paginas
+     */
     public int dimePaginas()
     {
         return Paginas;
     }
+    /**
+     * Permite acceder a los datos del lib
+     * @return una cadena con todos los detalles
+     */
     public String dimeDetalles()
     {
         String Detalles = "";
@@ -48,6 +61,10 @@ public class libro
         Detalles += Prestamos;
         return Detalles;
     }
+    /**
+     * Permite modificar el numero de referencia
+     * @Param Recibe el nuevo numero de referencia
+     */
     public void cambiaNumRef(String numRef)
     {
         if(numRef.length() == 3)
@@ -55,14 +72,25 @@ public class libro
              numeroDeReferencia = numRef;
         }
     }
+    /**
+     * Permite registrar el numero de referencia
+     * @return Regresa el numero de referencia
+     */
     public String dimeNumRef()
     {
         return numeroDeReferencia;
     }
+    /**
+     * Solicita un prestamo y aumenta en +1 la variable
+     */
     public void presta()
     {
         Prestamos += 1;
     }
+    /**
+     * Permite registrar el numero de prestamos
+     * @return Regresa el numero de prestamos
+     */
     public int dimePrestamos()
     {
         return Prestamos;
